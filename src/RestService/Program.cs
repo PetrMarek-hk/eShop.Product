7ced63c189c869c1dd3ee01930a669d528c231f5
Petr Marek <petr.marek@cz.kruk.eu>
2025-10-07 21:13:08 +0200

    Swagger V2, gitignore - SQL Lite files

--- src/RestService/Program.cs
+++ src/RestService/Program.cs
@@ -1,7 +1,9 @@
 using eShop.Product.ApplicationLogic;
 using eShop.Product.RestService.Initialization;
-
-
+using Microsoft.OpenApi.Models;
+using Asp.Versioning.ApiExplorer;
+using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,12 +12,44 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen(options =>
+//{
+//    //options.EnableAnnotations(); // This method is part of Swashbuckle.AspNetCore.Annotations
+//    //options.OperationFilter<Kruk.LegalCasesManager.Types.Enumerations.TypeEnumOperationFilter>();
+
+//    options.SwaggerDoc("v1", new OpenApiInfo
+//    {
+//        Version = "v1",
+//        Title = "eShop.Product.RestService",
+//        Description = "Demo project",
+//    });
+//});
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "eShop.Product.Restervice V1", Version = "v1" });
+    options.SwaggerDoc("v2", new OpenApiInfo { Title = "eShop.Product.Restervice V2", Version = "v2" });
+});
+// Add API versioning
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = new UrlSegmentApiVersionReader();
+});
 
 builder.Services
     .AddApplicationLogicServices()
     .AddAutoMapperConfiguration();
 
 var app = builder.Build();
+
+app.UseSwaggerUI(options =>
+{
+    //options.SwaggerEndpoint("/swagger/v1/swagger.json", "eShop.Product.Restervice V1");
+    options.SwaggerEndpoint("/swagger/v2/swagger.json", "eShop.Product.Restervice V2");
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
